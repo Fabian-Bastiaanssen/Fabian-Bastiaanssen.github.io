8650c1f12f969e5c33c05808ce773e011499f7d1
Fabian Bastiaanssen <122542835+Fabian-Bastiaanssen@users.noreply.github.com>
2024-04-22 18:01:39 +0100

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -15,8 +15,8 @@ titles.forEach(title => {
     // store the original word in the title's data attribute
     title.dataset.word = word.toUpperCase();
 });
-let guessWordInput = document.getElementById('guess-word').trim();
-let hangmanWord = document.getElementById('hangman-word').trim();
+let guessWordInput = document.getElementById('guess-word');
+let hangmanWord = document.getElementById('hangman-word');
 let word = hangmanWord.dataset.word;
 guessWordInput.setAttribute('maxlength', word.length);
 
